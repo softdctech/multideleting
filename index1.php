f8c0e843065e4c5d9c297e88107d458e27bb1d3c
softdctech <81722005+softdctech@users.noreply.github.com>
2021-04-01 06:35:11 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+
+<title>Links Page</title>
+
+</head>
+
+<body>
+
+<h2>Choose and delete selected links.</h2>
+
+
+
+<table width="400" border="0" cellspacing="1" cellpadding="0">
+<tr>
+<td><form name="form1" method="post" action="">
+<table width="400" border="0" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
+<td bgcolor="#FFFFFF">&nbsp;</td>
+<td colspan="3" bgcolor="#FFFFFF"><strong>Delete multiple links</strong> </td>
+</tr>
+<tr>
+<td align="center" bgcolor="#FFFFFF">#</td>
+<td align="center" bgcolor="#FFFFFF"><strong>Link ID</strong></td>
+<td align="center" bgcolor="#FFFFFF"><strong>Link Name</strong></td>
+<td align="center" bgcolor="#FFFFFF"><strong>Link URL</strong></td>
+</tr>
+
+
+
+<tr>
+<td align="center" bgcolor="#FFFFFF"><input name="checkbox" type="checkbox" value="<?php echo $row['link_id']; ?>"></td>
+<td bgcolor="#FFFFFF">cvbcvbcvb</td>
+<td bgcolor="#FFFFFF">vbvcbcb</td>
+<td bgcolor="#FFFFFF">bvcbcvb</td>
+</tr>
+
+<tr>
+<td colspan="4" align="center" bgcolor="#FFFFFF"><input name="delete" type="submit" value="Delete"></td>
+</tr>
+<table width="400" border="0" cellspacing="1" cellpadding="0">
+<tr>
+<td><form name="form1" method="post" action="">
+<table width="400" border="0" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
+ <tr>
+<td bgcolor="#FFFFFF">&nbsp;</td>
+<td colspan="3" bgcolor="#FFFFFF"><strong>Delete multiple links</strong> </td>
+</tr>
+<tr>
+<td align="center" bgcolor="#FFFFFF">#</td>
+<td align="center" bgcolor="#FFFFFF"><strong>Link ID</strong></td>
+<td align="center" bgcolor="#FFFFFF"><strong>Link Name</strong></td>
+<td align="center" bgcolor="#FFFFFF"><strong>Link URL</strong></td>
+ </tr>
+
+
+<tr>
+<td align="center" bgcolor="#FFFFFF"><input name="checkbox" type="checkbox" value="<?php echo $row['link_id']; ?>"></td>
+<td bgcolor="#FFFFFF">fgfffgfg</td>
+<td bgcolor="#FFFFFF">fdgdgdfgdg</td>
+<td bgcolor="#FFFFFF">gdgfgfg</td>
+</tr>
+
+<tr>
+<td colspan="4" align="center" bgcolor="#FFFFFF"><input name="delete" type="submit" value="Delete"></td>
+</tr>
+
+
+
+
+</table>
+</form>
+</td>
+</tr>
+</table>
+
+</body>
+
+</html>
